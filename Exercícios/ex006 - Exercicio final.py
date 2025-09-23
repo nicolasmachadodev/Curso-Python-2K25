ba4604930cdc3149cb9ccb3aedd4d6f4f1b3c321
Nicolas Eduardo "Nick <nicolasmachadodev@gmail.com>
2025-09-23 12:51:52 -0300

    update

--- Exercícios/ex006 - Exercicio final.py
+++ Exercícios/ex006 - Exercicio final.py
@@ -13,5 +13,31 @@ Ele deve atender aos seguintes requisitos:
 Obs: Este é o exercício de final do módulo, então aproveite para utilizar todos os recursos vistos até agora, como os funções, condições, loop, listas, etc.
 
 """
+timesDict = {}
+done = True
+def seeAll(timesDict):
+    for key in timesDict:
+        print(f"{key} - Jogadores: {len(timesDict[key]['Jogadores'])}")
+
+while done:
+    print("""Times de futebol.
+
+1 - Adicionar um novo time
+2 - Remover um time
+3 - Listar todos os times
+4 - Adicionar jogador
+5 - Remover um jogador
+6 - Listar jogadores de um time
+7 - Sair""")
+    
 
 
+    option = int(input("Selecione: "))
+    if option == 1:
+        newTime = input('Nome do novo time: ')
+        timesDict[newTime] = {"Jogadores": []}
+    elif option == 3:
+        seeAll(timesDict)
+    elif option == 7:
+        break;
+    
