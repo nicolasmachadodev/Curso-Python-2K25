c4ffc3164717cb10261120d4b0b5925909c4b42b
Nicolas Eduardo "Nick <nicolasmachadodev@gmail.com>
2025-09-23 23:44:21 -0300

    update

---
+++ 2 - Módulos/01-modulos.py
@@ -0,0 +1,6 @@
+import calc
+from calc import div
+
+print(calc.sum(2, 5))
+
+print(div(4, 2))
---
+++ 2 - Módulos/calc.py
@@ -0,0 +1,13 @@
+def sum(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mult(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        return "Erro: Divisão por zero não é permitida."
+    return a / b
