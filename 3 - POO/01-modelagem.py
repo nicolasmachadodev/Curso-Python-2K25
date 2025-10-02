dc77ace8bb5cc6e2e7a833ec6ed4b5316aa595d8
Nicolas Eduardo "Nick <nicolasmachadodev@gmail.com>
2025-10-01 23:49:59 -0300

    Introdução a programação orientada a objetos

---
+++ 3 - POO/01-modelagem.py
@@ -0,0 +1,18 @@
+class Pessoas:
+    def __init__(self, nome, idade, cpf):
+        self.nome = nome
+        self.idade = idade
+        self.cpf = cpf
+    
+    def return_name(self):
+        return self.nome
+
+    def logar_sistema(self):
+        print(f"{self.return_name()} esta logando no sistema.")
+
+p1 = Pessoas('Nicolas', 16, 10415218985)
+p2 = Pessoas('Emilly', 17, 68638361904)
+
+p1.logar_sistema()
+
+print(p2.idade)
